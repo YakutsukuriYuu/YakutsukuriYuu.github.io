c73809ec977e2de31ef3851270aeed242e2312f9
YakutsukuriYuu <qiansu_mu@163.com>
2024-06-05 10:04:29 +0800

    Auto commit

--- 考研/数据结构/test.cpp
+++ 考研/数据结构/test.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-
-    cout<<"hellworld"<<endl;
-
+    int n;
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cout<<i<<" ";
+    }
+    cout<<endl;
     return 0;
 }
